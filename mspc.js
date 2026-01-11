1f241758fa4fc1e212dcf4a67e59e8ac4ed95345
littledove-hue <lildovee758@gmail.com>
2026-01-12 02:39:38 +0530

    Update mspc.js

--- mspc.js
+++ mspc.js
@@ -20,7 +20,7 @@ const scripts = [
   { name: 'Maps Event', fn: runMapsEvent, envKey: 'LP_MAPS_URL' },
   { name: 'Slots Event', fn: runSlotsEvent, envKey: 'LP_SLOTS_URL' },
   { name: 'Memory Event', fn: runMemoryEvent, envKey: 'LP_MEMORY_URL' },
-  { name: 'Furniture Script', fn: runFurnitureScript, alwaysRun: true },
+  { name: 'Furniture Script', fn: runFurnitureScript, alwaysRun: false },
   { name: 'Stats Extractor', fn: runStatsExtractor, alwaysRun: true },
   { name: 'Daily Tasks', fn: runDailyTasks, alwaysRun: true },
   { name: 'Boyfriend Kiss', fn: runBoyfriendKiss, alwaysRun: true },
@@ -136,6 +136,7 @@ const scripts = [
   await browser.close();
   console.log(`\n🎉 All scripts done. Browser closed.`);
 })();
+
 
 
 
